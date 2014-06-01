2abc9d4e48f5780604e4d92ebfb2ed5dc98cc286
Joel Shin <jwshin@gmail.com>
2014-06-01 00:31:08 -0700

    added gitignore

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
-puts "\n---> lets try typhoeus"
+#require File.join(File.dirname(__FILE__), '..', 'client.rb')
+$LOAD_PATH << File.expand_path('..', __FILE__)
+#require 'customer'
+#require File.join(File.dirname(__FILE__), '..', 'services.rb')
+
+require 'client'
+
+puts "\n---> lets get a customer"
+
+begin
+ #cust = Customer.find_by_id 3
+ #puts "#{cust.inspect}"
+ puts "1. #{Customer.ancestors}"
+ puts "2. #{Customer.methods.inspect}"
+ Customer.base_uri = 'xxx'
+ puts "3. #{Customer.base_uri}"
+rescue => e
+ puts "ERROR!!!! #{e.inspect}"
+end
+
